fa03e11e420e46652cb9b131fe6f38067e315b90
okvo <43532955+okvo@users.noreply.github.com>
2020-08-07 11:17:14 +0200

    Add files via upload

---
+++ If.py
@@ -0,0 +1,20 @@
+#!/bin/python
+#https://www.hackerrank.com/challenges/py-if-else/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    if n % 2 != 0:
+        print ('Weird')
+    else:
+        if n >=2 and n<=5:
+            print ('Not Weird')
+        elif n>=6 and n<=20:
+            print ('Weird')
+        else:
+            print ('Not Weird') 
